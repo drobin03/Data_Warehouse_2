da42c288fd38f9216002771920ed3e2e9ce310ad
drobin03 <drobin03@uoguelph.ca>
2013-04-01 19:10:39 -0400

    Changed the parser, so that it is generic and can be used for any of the data sources. Also added the ability to add a data source.

--- Warehouse_Store.py
+++ Warehouse_Store.py
@@ -10,10 +10,28 @@ import MySQLdb
 from lxml import etree
 
 class Data:
-	def __init__(self, xml, schema):
+	def __init__(self, name, xml, schema, cursor):
 		self.xml = xml
 		self.schema = schema
 		self.values = []
+		self.rows = []
+		self.table_name = name
+		self.cursor = cursor
+		self.fields = []
+
+		#
+		# Get the table fields from the database
+		#
+		query = "SELECT column_name FROM information_schema.columns WHERE table_name='%s'" % (self.table_name)
+
+		try:
+			self.cursor.execute(query)
+			results = self.cursor.fetchall()
+		except (MySQLdb.OperationalError, MySQLdb.IntegrityError) as error:
+			print error
+			raise
+		for source in results:
+			self.fields.append("%s" % (source))
 
 	def validate(self):
 		#
@@ -27,48 +45,49 @@ class Data:
 			return false 
 
 	def parse(self):
-		print self.xml
+		parser = etree.XMLParser(remove_blank_text=True)
 		try:
-			doc = etree.fromstring(self.xml)
+			print self.xml
+			doc = etree.fromstring(self.xml, parser)
 		except:
 			print "Error reading XML file"
 			raise
 
+		current_row = 0
 		for element in doc.getiterator():
+
+			if (element.tag == "record"):
+				current_row += 1
+				self.rows.append([])
+
 			# Create a new DBdata object for the piece of data, and store the (field, value) pair.
-			if (element.text != None):
-				self.values.append(DBdata(element.tag, element.text))
+			# Only accept the data if the tag matches the field in the database.
+			if (element.tag in self.fields):
+				self.rows[current_row - 1].append(DBdata(element.tag, element.text))
+			else :
+				print "%s is not a valid field in the database" % (element.tag)
 		
 	def store(self, cursor):
-		# initialize an insert command
-		num_fields = 0
-		date_time_list = []
-		upload_list = []
-		download_list = []
-
-		# Sort the data that was parsed into fields and rows to put into the table.
-		for data in self.values :
-			num_fields += 1
-			if data.field == "time" :
-				date_time_list.append("'%s'" % (data.value))
-			elif data.field == "upload" :
-				upload_list.append(float(data.value))
-			elif data.field == "download" :
-				download_list.append(float(data.value))
-			else :
-				num_fields -= 1
-				print "Invalid field %s" % (data.field)
-
-		#
-		# Format the statements to store the data in the database
-		#
-
-		# Each row has 3 fields
-		num_rows = num_fields / 3
+		# initialize an insert command for each row
 		sql_statements = []
-		for i in range(0, num_rows) :
-			sql_statements.append("INSERT INTO `cis3760`.`Internet_Throughput` (`datetime`, `upload`, `download`) VALUES (%s, %f, %f)" % (date_time_list[i], upload_list[i], download_list[i]))
-		
+		for row in self.rows :
+			statement = "INSERT INTO `%s`.`%s` (" % ("cis3760", self.table_name)
+			for field in self.fields :
+				statement += "`%s`," % (field)
+			statement = statement.rstrip(",")
+			statement += ") VALUES ("
+
+			for data in row:
+				if (isinstance(data.value, str) ):
+					val = "'%s'" % (data.value)
+				else :
+					val = data.value
+
+				statement += "%s," % (str(val))
+			statement = statement.rstrip(",")
+			statement += ")"
+
+			sql_statements.append(statement)
 
 		#
 		# Store the data
@@ -87,6 +106,15 @@ class DBdata:
 	def __init__(self, data_field, data_value):
 		self.field = data_field
 		self.value = data_value
+
+class Table:
+	def __init__(self):
+		self.name = ""
+		self.fields = {}
+
+	def add_field(self, name, value):
+		self.fields[name] = value;
+
 
 class DataObserver:
 	#
@@ -115,7 +143,7 @@ class DataObserver:
 
 	def store_data(self, data_name, xml):
 		schema = self.lookup_schema(data_name)
-		data_to_add = Data(xml, schema)
+		data_to_add = Data(data_name, xml, schema, self.cursor)
 
 		# NOT Validating right now. We need a schema from the data sources before we can do that.
 		#data_to_add.validate()
@@ -125,13 +153,53 @@ class DataObserver:
 		self.db.commit()
 		self.db.close()
 
+	# This function creates a new table in the database.
+	# The table name will be set to 'name' and the structure of
+	# the table will follow the definitions in the XML schema 'schema'
+	def add_data_source(self, name, schema):
+		new_table = Table()
+
+		try:
+			doc = etree.fromstring(schema)
+		except:
+			print "Error reading SCHEMA"
+			raise
+
+		# Get the Field names and types from the xsd.
+		for element in doc.getiterator():
+			if (element.get("type") != None):
+				# Trim the 'xs:' from the beginning of the types.
+				trimmed_type = element.get("type")[3:]
+				new_table.add_field(element.get("name"), trimmed_type)
+
+		# Create a new table that follows the data read from the schema.
+		sql_statement = "CREATE TABLE `%s` (" % (name) 
+		# Add each of the column names, and data types.
+		for field in new_table.fields.keys():
+			sql_statement += " %s %s," % (field, new_table.fields[field])
+		sql_statement = sql_statement.rstrip(",")
+		sql_statement += " )"
+		
+		print sql_statement
+
+		try:
+			self.cursor.execute(sql_statement)
+			results = self.cursor.fetchall()
+		except (MySQLdb.OperationalError, MySQLdb.IntegrityError) as error:
+			print error
+
 if __name__ == "__main__": 
 
 	#
 	# This function is used to test our store functionality. The data sources
 	# module will eventually perform these functions.
 	#
-	test_xml = "<xml><record><upload>3.5</upload><download>3.4</download><time>2010-01-23 23:52:59</time></record><record><upload>2.4</upload><download>2.5</download><time>2011-02-28 23:00:04</time></record></xml>"
-
 	observer = DataObserver()
-	observer.store_data("Internet_Throughput", test_xml)
+	
+	test_xml_file = open("./test_big_brother_data.xml", "r")
+	test_xml = test_xml_file.read()
+
+	schema_file = open("./big_brother.xsd", "r")
+	observer.add_data_source("Big Brother", schema_file.read())
+
+	#observer.store_data("Internet_Throughput", test_xml)
