1568d45aad97edae590e4a7bbbb69e52d4488fa7
linux <linux@ubuntu.(none)>
2013-04-01 22:18:16 -0700

    reading from a file quite fragile

--- Warehouse_Consolidate.py
+++ Warehouse_Consolidate.py
@@ -10,12 +10,18 @@ import string
 class Consolidator:
 
     def main():
-        startDate = "'2013-03-27 00:00:00'"
-        endDate = "'2013-03-28 23:59:59'"
+        data = open('config.txt', 'r') 
+        buff = data.readline().rstrip()
+        startDate = data.readline().rstrip()
+        buf = data.readline().rstrip()
+        endDate = data.readline().rstrip()
+        print startDate
+        print endDate
         summaryU = 0.0
         summary = 0.0
         table = "Internet_Throughput"
         db = MySQLdb.connect("www.farm-to-fork.ca","manager","waterfall","cis3760") #IP, USERNAME, PASS, DB NAME
+        db.autocommit(1)#enable auto commit
         cursor = db.cursor()
         # execute SQL query using execute() method.
         cursor.execute("SELECT VERSION()")
@@ -23,15 +29,17 @@ class Consolidator:
         data = cursor.fetchone()
         print "Database version : %s " % data
         
-        sq1 = "SELECT * FROM `Internet_Throughput` WHERE `datetime` BETWEEN "
+        sq1 = "SELECT * FROM `Internet_Throughput` WHERE `datetime` BETWEEN '"
         sq1 += startDate
-        sq1 +=" AND "
+        sq1 +="' AND '"
         sq1 += endDate
-        sq2 = "INSERT INTO `cis3760`.`Consolidation` (`start_date`, `end_date`, `upload`, `download`) VALUES ("
+        sq1 += "';"
+        print sq1
+        sq2 = "INSERT INTO `cis3760`.`Consolidation` (`start_date`, `end_date`, `upload`, `download`) VALUES ('"
         sq2 += startDate
-        sq2 += ", "
+        sq2 += "', '"
         sq2 += endDate
-        sq2 += ", '"
+        sq2 += "', '"
         try:
             cursor.execute(sq1)
             results = cursor.fetchall()
@@ -52,11 +60,11 @@ class Consolidator:
             sq2 += "', '"
             sq2 += str(summary)
             sq2 += "');"
-            print sq2           
-
-            cursor.execute(sq2)
-        except:
-            print "Error: unable to fetch data"
+                    
+            print sq2  
+            cursor.execute(sq2)  
+        except (MySQLdb.OperationalError, MySQLdb.IntegrityError) as error:
+                print error
         # disconnect from server
         db.close()
     if __name__ == "__main__":
